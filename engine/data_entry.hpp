d6b2f1a4c98b746b27151d44109eb12f2700b46d
Jiayu Wu <jiayu.wu@intel.com>
2021-09-26 23:56:40 +0800

    Fix DataEntry Checksum: Handle corrupted key and value size
    Signed-off-by: Jiayu Wu <jiayu.wu@intel.com>

--- engine/data_entry.hpp
+++ engine/data_entry.hpp
@@ -48,6 +48,7 @@ struct DataHeader {
   DataHeader(uint32_t c, uint32_t s) : checksum(c), b_size(s) {}
 
   uint32_t checksum;
+  // entry size in blocks
   uint32_t b_size;
 };
 
@@ -66,11 +67,20 @@ struct DataEntry {
   uint32_t v_size;
   char data[0];
 
+  // Calculate checksum, a valid checksum is always > 0
   // make sure there is data followed in data[0]
-  uint32_t Checksum() {
-    return get_checksum((char *)this + sizeof(DataHeader),
-                        sizeof(DataEntry) - sizeof(DataHeader) + k_size +
-                            v_size);
+  // "pmem_block_size" is used for checking validation of k_size and v_size, as
+  // data entry may be left corrupted
+  // TODO: store actual size in data header so we don't need this parameter
+  uint32_t Checksum(uint32_t pmem_block_size) {
+    uint32_t checksum_size =
+        k_size + v_size + sizeof(DataEntry) - sizeof(DataHeader);
+    if (checksum_size + sizeof(DataHeader) >= pmem_block_size * header.b_size) {
+      // Fixme: return error instead of 0
+      return 0;
+    }
+    return std::max(
+        get_checksum((char *)this + sizeof(DataHeader), checksum_size), 1UL);
   }
 
   // make sure there is data followed in data[0]
@@ -93,35 +103,30 @@ struct DLDataEntry : public DataEntry {
   uint64_t next;
   char data[0];
 
+  // Calculate checksum, a valid checksum is always > 0
   // make sure there is data followed in data[0]
-  uint32_t Checksum() {
-    return get_checksum((char *)this + sizeof(DataHeader),
-                        sizeof(DataEntry) - sizeof(DataHeader)) +
-           get_checksum(data, v_size + k_size);
+  // "pmem_block_size" is used for checking validation of k_size and v_size, as
+  // data entry may be left corrupted
+  // TODO: store actual size in data header so we don't need this parameter
+  uint32_t Checksum(uint32_t pmem_block_size) {
+    uint32_t meta_checksum_size =
+        sizeof(DataEntry) - sizeof(DataHeader) /* we don't checksum pointers */;
+    uint32_t data_checksum_size = k_size + v_size;
+    if (meta_checksum_size + data_checksum_size + 16 /*pointers*/ +
+            sizeof(DataHeader) >
+        pmem_block_size * header.b_size) {
+      return 0;
+    }
+    return std::max(get_checksum((char *)this + sizeof(DataHeader),
+                                 sizeof(DataEntry) - sizeof(DataHeader)) +
+                        get_checksum(data, v_size + k_size),
+                    1UL);
   }
 
   // make sure there is data followed in data[0]
   pmem::obj::string_view Key() { return pmem::obj::string_view(data, k_size); }
 
   // make sure there is data followed in data[0]
-  pmem::obj::string_view Value() {
-    return pmem::obj::string_view(data + k_size, v_size);
-  }
-};
-
-// Singly linked
-struct SLDataEntry : public DataEntry {
-  uint64_t next;
-  char data[0];
-
-  uint32_t Checksum() {
-    return get_checksum((char *)this + sizeof(DataHeader),
-                        sizeof(DataEntry) - sizeof(DataHeader)) +
-           get_checksum(data, v_size + k_size);
-  }
-
-  pmem::obj::string_view Key() { return pmem::obj::string_view(data, k_size); }
-
   pmem::obj::string_view Value() {
     return pmem::obj::string_view(data + k_size, v_size);
   }
