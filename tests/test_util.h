839162b367ab4cdffe70ab05cd01a8d03c60c3d5
ZiyanShi <ziyan.shi@intel.com>
2021-09-30 18:20:13 -0400

    no static function in header and ProgressBar class
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

--- tests/test_util.h
+++ tests/test_util.h
@@ -4,6 +4,7 @@
 
 #include <atomic>
 #include <iostream>
+#include <numeric>
 #include <vector>
 #include <random>
 #include <string>
@@ -11,11 +12,11 @@
 #include <iomanip>
 
 /* Create a string that contains 8 bytes from uint64_t. */
-static inline std::string uint64_to_string(uint64_t &key) {
+inline std::string uint64_to_string(uint64_t &key) {
   return std::string(reinterpret_cast<const char *>(&key), 8);
 }
 
-static inline void random_str(char *str, unsigned int size) {
+inline void random_str(char *str, unsigned int size) {
   for (unsigned int i = 0; i < size; i++) {
     switch (rand() % 3) {
     case 0:
@@ -35,7 +36,7 @@ static inline void random_str(char *str, unsigned int size) {
 }
 
 // Return a string of length len with random characters in ['a', 'z']
-static inline std::string GetRandomString(size_t len)
+inline std::string GetRandomString(size_t len)
 {
   static std::default_random_engine re;
   std::string str;
@@ -46,7 +47,7 @@ static inline std::string GetRandomString(size_t len)
 }
 
 // Return a string of length in [min_len, max_len] with random characters in ['a', 'z']
-static inline std::string GetRandomString(size_t min_len, size_t max_len)
+inline std::string GetRandomString(size_t min_len, size_t max_len)
 {
   static std::default_random_engine re;
   size_t len = 0;
@@ -56,7 +57,7 @@ static inline std::string GetRandomString(size_t min_len, size_t max_len)
 }
 
 
-static inline void LaunchNThreads(int n_thread, std::function<void(int tid)> func,
+inline void LaunchNThreads(int n_thread, std::function<void(int tid)> func,
                     uint32_t id_start = 0) {
   std::vector<std::thread> ts;
   for (int i = id_start; i < id_start + n_thread; i++) {
@@ -66,7 +67,7 @@ static inline void LaunchNThreads(int n_thread, std::function<void(int tid)> fun
     t.join();
 }
 
-static void ShowProgress(std::ostream& os, int progress, int total, size_t len_bar = 50, char symbol_done = '#', char symbol_fill = '-')
+void ShowProgress(std::ostream& os, int progress, int total, size_t len_bar = 50, char symbol_done = '#', char symbol_fill = '-')
 {
   assert(0 <= progress && progress <= total);
   int step = total / len_bar;
@@ -91,3 +92,90 @@ static void ShowProgress(std::ostream& os, int progress, int total, size_t len_b
   if (progress == total)
     os << std::endl;
 }
+
+class ProgressBar
+{
+private:
+  std::ostream& _out_stream_;
+  std::string _tag_;
+  int _total_progress_;   
+  int _current_progress_;
+  int _bar_length_;
+  int _step_;
+
+  bool _finished_{ false };
+
+  static constexpr char _symbol_done_{'#'};
+  static constexpr char _symbol_fill_{'-'};
+
+public:
+  explicit ProgressBar(std::ostream& out, std::string tag, int total_progress, int bar_length = 50) :
+    _out_stream_{out},
+    _tag_{tag},
+    _total_progress_{total_progress},
+    _current_progress_{0},
+    _bar_length_{bar_length},
+    _step_{total_progress/bar_length}
+  {
+    assert(_total_progress_ > 0);
+    assert(_bar_length_ > 0);
+    if (_step_ == 0)
+    {
+      _step_ = 1;
+      _bar_length_ = total_progress;
+    }
+
+    showProgress();
+  }
+
+  void Update(int current_progress)
+  {
+    assert(!_finished_ && "Trying to update a completed progress!");
+    assert(_current_progress_ < current_progress && current_progress <= _total_progress_);
+
+    _current_progress_ = current_progress;
+    if (_current_progress_ == _total_progress_)
+      _finished_ = true;
+
+    showProgress();
+  }
+
+  ~ProgressBar()
+  {
+    if(!_finished_)
+    {
+      _finished_ = true;
+      showProgress();
+    }
+  }
+
+private:
+  void showProgress()
+  {
+    assert(0 <= _current_progress_ && _current_progress_ <= _total_progress_);
+
+    _out_stream_ 
+      << "\r"
+      << _tag_
+      << std::setw(10) << std::right << _current_progress_
+      <<  "/"
+      << std::setw(10) << std::left << _total_progress_ 
+      << "\t"
+      << "[";
+
+      {
+        int n_step_done = _current_progress_ / _step_;
+        for (size_t i = 0; i < n_step_done; i++)
+          _out_stream_ << _symbol_done_;
+        for (size_t i = 0; i < _bar_length_ - n_step_done; i++)
+          _out_stream_ << _symbol_fill_;
+      }
+
+    _out_stream_
+      << "]"
+      << std::flush;
+
+    if (_finished_)
+      _out_stream_ << std::endl;
+  }
+};
