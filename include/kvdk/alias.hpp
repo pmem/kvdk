87aeefc9ce71f9860a9a096cd3e9a16ce6375852
ZiyanShi <ziyan.shi@intel.com>
2021-10-28 05:17:21 -0400

    aliasing types
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

---
+++ include/kvdk/alias.hpp
@@ -0,0 +1,10 @@
+#include <cinttypes>
+
+#include "../libpmemobj++/string_view.hpp"
+
+namespace KVDK_NAMESPACE {
+using StringView = pmem::obj::string_view;
+using PMemOffsetType = std::uint64_t;
+using TimeStampType = std::uint64_t;
+using CollectionIDType = std::uint64_t;
+} // namespace KVDK_NAMESPACE
