ca50392dd254d70175869328af26896caeb8481e
ZiyanShi <ziyan.shi@intel.com>
2021-12-21 23:45:25 +0800

    whatever
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

--- engine/macros.hpp
+++ engine/macros.hpp
@@ -4,9 +4,14 @@
   std::hex << std::setfill('0') << std::setw(sizeof(decltype(x)) * 2) << x     \
            << std::dec
 
+#if DEBUG_LEVEL > 0
 #define kvdk_assert(cond, msg)                                                 \
   {                                                                            \
     assert((cond) && msg);                                                     \
     if (!(cond))                                                               \
       throw std::runtime_error{msg};                                           \
   }
+#else
+#define kvdk_assert(cond, msg)                                                 \
+  {}
+#endif
