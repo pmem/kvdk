78dbf3afe2cbf3c9375a5ca289f4ef6e7005a37b
ZiyanShi <ziyan.shi@intel.com>
2022-01-10 01:56:05 -0500

    simplify code
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

--- include/kvdk/status.h
+++ include/kvdk/status.h
@@ -21,23 +21,20 @@
 extern "C" {
 #endif
 
-#define FOREACH_ENUM(GEN)                                                      \
-  GEN(Ok)                                                                      \
-  GEN(NotFound)                                                                \
-  GEN(MemoryOverflow)                                                          \
-  GEN(PmemOverflow)                                                            \
-  GEN(NotSupported)                                                            \
-  GEN(MapError)                                                                \
-  GEN(BatchOverflow)                                                           \
-  GEN(TooManyWriteThreads)                                                     \
-  GEN(InvalidDataSize)                                                         \
-  GEN(IOError)                                                                 \
-  GEN(InvalidConfiguration)                                                    \
-  GEN(Abort)
-#define GENERATE_ENUM(ENUM) ENUM,
-#define GENERATE_STRING(STRING) #STRING,
-
-typedef enum { FOREACH_ENUM(GENERATE_ENUM) } KVDKStatus;
+typedef enum {
+  Ok,
+  NotFound,
+  MemoryOverflow,
+  PmemOverflow,
+  NotSupported,
+  MapError,
+  BatchOverflow,
+  TooManyWriteThreads,
+  InvalidDataSize,
+  IOError,
+  InvalidConfiguration,
+  Abort
+} KVDKStatus;
 
 #ifdef __cplusplus
 } /* end extern "C" */
