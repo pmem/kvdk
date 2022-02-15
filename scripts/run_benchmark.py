5e0332b37174848e0ec5a29cf678d4872cde5e86
ZiyanShi <ziyan.shi@intel.com>
2022-02-15 23:09:28 +0800

    amend
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -11,7 +11,7 @@ num_thread = 64
 value_sizes = [120]
 # constant: value size always be "value_size",
 # random: value size uniformly distributed in [1, value_size]
-value_size_distributions = ['random']
+value_size_distributions = ['constant']
 timeout = 30                          # For operations other than fill
 populate_on_fill = 1                  # For fill only
 pmem_size = 384 * 1024 * 1024 * 1024  # we need enough space to test insert
