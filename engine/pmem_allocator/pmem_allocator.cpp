5d201acdd82334cbd630a53724331e54f067f585
ZiyanShi <ziyan.shi@intel.com>
2022-03-10 17:33:04 +0800

    amend
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

--- engine/pmem_allocator/pmem_allocator.cpp
+++ engine/pmem_allocator/pmem_allocator.cpp
@@ -301,7 +301,9 @@ SpaceEntry PMEMAllocator::Allocate(uint64_t size) {
 }
 
 void PMEMAllocator::persistSpaceEntry(PMemOffsetType offset, uint64_t size) {
-  DataEntry padding{0, size, TimeStampType{}, RecordType::Padding, 0, 0};
+  std::uint32_t sz = static_cast<std::uint32_t>(size);
+  kvdk_assert(size == static_cast<std::uint64_t>(sz), "Integer Overflow!");
+  DataEntry padding{0, sz, TimeStampType{}, RecordType::Padding, 0, 0};
   pmem_memcpy_persist(offset2addr_checked(offset), &padding, sizeof(DataEntry));
 }
 
