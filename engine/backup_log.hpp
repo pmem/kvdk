384d17db27ff7727e9d00c40f3cb5a0936e68c11
Jiayu Wu <jiayu.wu@intel.com>
2022-05-15 23:12:20 +0800

    trivial
    Signed-off-by: Jiayu Wu <jiayu.wu@intel.com>

--- engine/backup_log.hpp
+++ engine/backup_log.hpp
@@ -16,37 +16,18 @@ namespace KVDK_NAMESPACE {
 
 constexpr size_t kMaxBackupLogBufferSize = 128 << 20;
 
+enum BackupStatus {
+  Processing = 0,
+  Finished,
+  Error,
+};
+
+// Persist/Read backup of a instead as log-like manner
+// Format:
+// status | string record 1 | string record 2 | sorted header record 1 | sorted
+// elems of header 1 | ... sorted header n | sorted elems of header n | ... |
+// string record n| ...
 class BackupLog {
- private:
-  std::string delta_;
-  char* log_file_;
-  size_t file_size_;
-  int fd_{-1};
-
-  Status persistDelta() {
-    GlobalLogger.Debug("call persist delta\n");
-    if (ftruncate64(fd_, file_size_ + delta_.size())) {
-      GlobalLogger.Error("Allocate space for backup log file error: %s\n",
-                         strerror(errno));
-      return Status::IOError;
-    }
-    log_file_ = (char*)mmap(log_file_, file_size_ + delta_.size(),
-                            PROT_WRITE | PROT_READ, MAP_SHARED, fd_, 0);
-
-    if (log_file_ == nullptr) {
-      GlobalLogger.Error("Map backup log file error: %s\n", strerror(errno));
-      return Status::IOError;
-    }
-    GlobalLogger.Debug("call persist delta memcpy\n");
-    memcpy(log_file_ + file_size_, delta_.data(), delta_.size());
-    GlobalLogger.Debug("call persist delta memcpy ok\n");
-    file_size_ += delta_.size();
-    delta_.clear();
-    munmap(log_file_, file_size_);
-    GlobalLogger.Debug("call persist delta success\n");
-    return Status::Ok;
-  }
-
  public:
   struct LogRecord {
     RecordType type;
@@ -95,6 +76,11 @@ class BackupLog {
 
   // Init a new backup log
   Status Init(const std::string& backup_log) {
+    if (file_exist(backup_log)) {
+      GlobalLogger.Error("Init backup log %s error: file already exist\n",
+                         backup_log.c_str());
+      return Status::Abort;
+    }
     fd_ = open(backup_log.c_str(), O_CREAT | O_RDWR, 0666);
     if (fd_ >= 0) {
       log_file_ =
@@ -109,7 +95,7 @@ class BackupLog {
     return Status::Ok;
   }
 
-  // Open a existing backup log
+  // Open a existing backup log read-only
   Status Open(const std::string& backup_log) {
     fd_ = open(backup_log.c_str(), O_RDWR, 0666);
     if (fd_ >= 0) {
@@ -125,6 +111,7 @@ class BackupLog {
     }
     return Status::Ok;
   }
+
   // Append a record to backup log
   void Append(RecordType type, const StringView& key, const StringView& val) {
     AppendUint32(&delta_, type);
@@ -136,11 +123,42 @@ class BackupLog {
   }
 
   Status Finish() {
+    // TODO jiayu add a finish mark or checksum
     Status s = persistDelta();
     if (s != Status::Ok) {
       return s;
     }
     return Status::Ok;
   }
+
+ private:
+  Status persistDelta() {
+    GlobalLogger.Debug("call persist delta\n");
+    if (ftruncate64(fd_, file_size_ + delta_.size())) {
+      GlobalLogger.Error("Allocate space for backup log file error: %s\n",
+                         strerror(errno));
+      return Status::IOError;
+    }
+    log_file_ = (char*)mmap(log_file_, file_size_ + delta_.size(),
+                            PROT_WRITE | PROT_READ, MAP_SHARED, fd_, 0);
+
+    if (log_file_ == nullptr) {
+      GlobalLogger.Error("Map backup log file error: %s\n", strerror(errno));
+      return Status::IOError;
+    }
+    GlobalLogger.Debug("call persist delta memcpy\n");
+    memcpy(log_file_ + file_size_, delta_.data(), delta_.size());
+    GlobalLogger.Debug("call persist delta memcpy ok\n");
+    file_size_ += delta_.size();
+    delta_.clear();
+    munmap(log_file_, file_size_);
+    GlobalLogger.Debug("call persist delta success\n");
+    return Status::Ok;
+  }
+
+  std::string delta_{};
+  char* log_file_{nullptr};
+  size_t file_size_{0};
+  int fd_{-1};
 };
 }  // namespace KVDK_NAMESPACE
