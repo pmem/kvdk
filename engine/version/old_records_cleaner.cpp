14139828cd411b8a0ff85c0158e64747a54b16a7
Jiayu Wu <jiayu.wu@intel.com>
2022-05-19 14:47:11 +0800

    trivial
    Signed-off-by: Jiayu Wu <jiayu.wu@intel.com>

--- engine/version/old_records_cleaner.cpp
+++ engine/version/old_records_cleaner.cpp
@@ -380,11 +380,12 @@ SpaceEntry OldRecordsCleaner::purgeOldDeleteRecord(
           }
         }
 
-        bool ret = Skiplist::Remove(
+        bool success = Skiplist::Remove(
             static_cast<DLRecord*>(old_delete_record.pmem_delete_record),
             dram_node, kv_engine_->pmem_allocator_.get(),
             kv_engine_->skiplist_locks_.get());
-        kvdk_assert(ret, "Must be success as we already checked linkage");
+        kvdk_assert(success, "Must be success as we already checked linkage");
+        static_cast<DLRecord*>(old_delete_record.pmem_delete_record)->Destroy();
 
         if (hash_entry_ref) {
           // Erase hash entry after successfully purge record
