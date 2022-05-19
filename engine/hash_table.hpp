5cc230875018f397cffb0b1031346bf5e3957a48
ZiyanShi <ziyan.shi@intel.com>
2022-05-19 18:05:20 +0800

    amend
    Signed-off-by: ZiyanShi <ziyan.shi@intel.com>

--- engine/hash_table.hpp
+++ engine/hash_table.hpp
@@ -228,6 +228,22 @@ class HashTable {
     return guard;
   }
 
+  std::vector<std::unique_lock<SpinMutex>> RangeLock(
+      std::vector<std::string> const& keys) {
+    std::vector<SpinMutex*> spins;
+    for (auto const& key : keys) {
+      spins.push_back(GetHint(key).spin);
+    }
+    std::sort(spins.begin(), spins.end());
+    auto end = std::unique(spins.begin(), spins.end());
+
+    std::vector<std::unique_lock<SpinMutex>> guard;
+    for (auto iter = spins.begin(); iter != end; ++iter) {
+      guard.emplace_back(**iter);
+    }
+    return guard;
+  }
+
   HashTableIterator GetIterator();
 
  private:
