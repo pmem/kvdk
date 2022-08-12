7ed2ee42ff47da689a3122f9c562c536bcad5fa1
Jiayu Wu <jiayu.wu@intel.com>
2022-08-12 16:51:18 +0800

    trivial
    Signed-off-by: Jiayu Wu <jiayu.wu@intel.com>

--- engine/sorted_collection/rebuilder.cpp
+++ engine/sorted_collection/rebuilder.cpp
@@ -26,19 +26,21 @@ SortedCollectionRebuilder::SortedCollectionRebuilder(
 SortedCollectionRebuilder::RebuildResult SortedCollectionRebuilder::Rebuild() {
   RebuildResult ret;
   ret.s = initRebuildLists();
-  if (ret.s == Status::Ok && rebuild_skiplits_.size() > 0) {
-    if (segment_based_rebuild_) {
-      ret.s = segmentBasedIndexRebuild();
-    } else {
-      ret.s = listBasedIndexRebuild();
-    }
+  if (ret.s == Status::Ok) {
+    if (rebuild_skiplits_.size() > 0) {
+      if (segment_based_rebuild_) {
+        ret.s = segmentBasedIndexRebuild();
+      } else {
+        ret.s = listBasedIndexRebuild();
+      }
 
-    if (ret.s == Status::Ok) {
-      ret.max_id = max_recovered_id_;
-      ret.rebuild_skiplits.swap(rebuild_skiplits_);
+      if (ret.s == Status::Ok) {
+        ret.max_id = max_recovered_id_;
+        ret.rebuild_skiplits.swap(rebuild_skiplits_);
+      }
     }
+    cleanInvalidRecords();
   }
-  cleanInvalidRecords();
   return ret;
 }
 
