651815be8c59279d690d26463c595cb50c276354
Yu, Peng <peng.yu@intel.com>
2022-09-01 10:54:03 +0800

    Avoid use of write buffer when write data record  when WITH_PMEM=OFF
    Signed-off-by: Yu, Peng <peng.yu@intel.com>

--- engine/data_record.cpp
+++ engine/data_record.cpp
@@ -5,40 +5,41 @@
 #include "data_record.hpp"
 
 namespace KVDK_NAMESPACE {
+#ifdef KVDK_WITH_PMEM
 // use buffer to acc nt-write
 thread_local std::string thread_data_buffer;
 static constexpr int kDataBufferSize = 1024 * 1024;
+#endif
 
 StringRecord* StringRecord::PersistStringRecord(
     void* addr, uint32_t record_size, TimestampType timestamp, RecordType type,
     RecordStatus status, MemoryOffsetType old_version, const StringView& key,
     const StringView& value, ExpireTimeType expired_time) {
-  void* data_cpy_target;
+  void* data_cpy_target = addr;
+  bool with_buffer = false;
   auto write_size = key.size() + value.size() + sizeof(StringRecord);
-  bool with_buffer = write_size <= kDataBufferSize;
+
+#ifdef KVDK_WITH_PMEM
+  with_buffer = write_size <= kDataBufferSize;
   if (with_buffer) {
     if (thread_data_buffer.empty()) {
       thread_data_buffer.resize(kDataBufferSize);
     }
     data_cpy_target = &thread_data_buffer[0];
-  } else {
-    data_cpy_target = addr;
   }
+#endif
+
   StringRecord::ConstructStringRecord(data_cpy_target, record_size, timestamp,
                                       type, status, old_version, key, value,
                                       expired_time);
-  if (with_buffer) {
 #ifdef KVDK_WITH_PMEM
+  if (with_buffer) {
     pmem_memcpy(addr, data_cpy_target, write_size, PMEM_F_MEM_NONTEMPORAL);
     pmem_drain();
-#else
-    memcpy(addr, data_cpy_target, write_size);
-#endif
   } else {
-#ifdef KVDK_WITH_PMEM
     pmem_persist(addr, write_size);
-#endif
   }
+#endif
 
   return static_cast<StringRecord*>(addr);
 }
@@ -48,32 +49,31 @@ DLRecord* DLRecord::PersistDLRecord(
     RecordStatus status, MemoryOffsetType old_version, MemoryOffsetType prev,
     MemoryOffsetType next, const StringView& key, const StringView& value,
     ExpireTimeType expired_time) {
-  void* data_cpy_target;
+  void* data_cpy_target = addr;
+  bool with_buffer = false;
   auto write_size = key.size() + value.size() + sizeof(DLRecord);
-  bool with_buffer = write_size <= kDataBufferSize;
+
+#ifdef KVDK_WITH_PMEM
+  with_buffer = write_size <= kDataBufferSize;
   if (with_buffer) {
     if (thread_data_buffer.empty()) {
       thread_data_buffer.resize(kDataBufferSize);
     }
     data_cpy_target = &thread_data_buffer[0];
-  } else {
-    data_cpy_target = addr;
   }
+#endif
+
   DLRecord::ConstructDLRecord(data_cpy_target, record_size, timestamp, type,
                               status, old_version, prev, next, key, value,
                               expired_time);
-  if (with_buffer) {
 #ifdef KVDK_WITH_PMEM
+  if (with_buffer) {
     pmem_memcpy(addr, data_cpy_target, write_size, PMEM_F_MEM_NONTEMPORAL);
     pmem_drain();
-#else
-    memcpy(addr, data_cpy_target, write_size);
-#endif
   } else {
-#ifdef KVDK_WITH_PMEM
     pmem_persist(addr, write_size);
-#endif
   }
+#endif
 
   return static_cast<DLRecord*>(addr);
 }
