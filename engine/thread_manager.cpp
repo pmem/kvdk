ed69cee30a1c76d6d01994e0a85e02c582a91def
Jiayu Wu <jiayu.wu@intel.com>
2022-09-26 22:30:26 +0800

    thread id pool
    Signed-off-by: Jiayu Wu <jiayu.wu@intel.com>

--- engine/thread_manager.cpp
+++ engine/thread_manager.cpp
@@ -8,13 +8,42 @@
 
 namespace KVDK_NAMESPACE {
 
-ThreadManager ThreadManager::manager_;
+constexpr size_t kMaxRecycleID = 1024;
+
+std::shared_ptr<ThreadManager> ThreadManager::manager_(new ThreadManager);
+
+Thread::~Thread() {
+  if (manager != nullptr) {
+    manager->Release(*this);
+  }
+}
 
 void ThreadManager::MaybeInitThread(Thread& t) {
   if (t.id < 0) {
+    if (!recycle_id_.empty()) {
+      std::lock_guard<SpinMutex> lg(spin_);
+      if (recycle_id_.empty()) {
+        auto it = recycle_id_.begin();
+        t.manager = shared_from_this();
+        t.id = *it;
+        recycle_id_.erase(it);
+        return;
+      }
+    }
     int id = ids_.fetch_add(1, std::memory_order_relaxed);
+    t.manager = shared_from_this();
     t.id = id;
   }
+}
+
+void ThreadManager::Release(Thread& t) {
+  if (t.manager.get() == this && t.id >= 0 &&
+      recycle_id_.size() < kMaxRecycleID) {
+    std::lock_guard<SpinMutex> lg(spin_);
+    recycle_id_.insert(t.id);
+  }
+  t.id = -1;
+  t.manager = nullptr;
 }
 
 thread_local Thread this_thread;
--- engine/thread_manager.hpp
+++ engine/thread_manager.hpp
@@ -17,26 +17,31 @@ class ThreadManager;
 
 struct Thread {
  public:
-  Thread() : id(-1) {}
+  Thread() : id(-1), manager(nullptr) {}
   int64_t id;
+  std::shared_ptr<ThreadManager> manager;
+
+  ~Thread();
 };
 
 extern thread_local Thread this_thread;
 
 class ThreadManager : public std::enable_shared_from_this<ThreadManager> {
  public:
-  static ThreadManager* Get() { return &manager_; }
+  static ThreadManager* Get() { return manager_.get(); }
   static int64_t ThreadID() {
     Get()->MaybeInitThread(this_thread);
     return this_thread.id;
   }
   void MaybeInitThread(Thread& t);
+  void Release(Thread& t);
 
  private:
-  ThreadManager() : ids_(0) {}
+  ThreadManager() : ids_(0), recycle_id_(), spin_() {}
 
-  static ThreadManager manager_;
+  static std::shared_ptr<ThreadManager> manager_;
   std::atomic<int64_t> ids_;
+  std::unordered_set<uint32_t> recycle_id_;
   SpinMutex spin_;
 };
 
